b211877967c3c6bbdac31192106cc6a8eda9714a
vgabin <gabin.vallee@outlook.fr>
2025-06-04 16:05:01 +0200

    Add table into the dashboard + fix join call

--- web/server.js
+++ web/server.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const session = require("express-session");
+const { getAllLastestPayments } = require("./src/services/payments")
+const { getAllSubscriptions } = require("./src/services/subscriptions")
 const app = express();
 
 const authenticationRoutes = require("./src/routes/authentication");
@@ -21,6 +23,30 @@ app.use(express.static("public"));
 app.get("/", (req, res) => {
   if (req.session.user) {
     res.render("index", { user: req.session.user });
+  } else {
+    res.render("login", {
+      clientId: process.env.DISCORD_ID,
+      redirectUri: process.env.REDIRECT_URI,
+    });
+  }
+});
+
+app.get("/abonnements", async (req, res) => {
+  if (req.session.user) {
+    const subscriptions = await getAllSubscriptions();
+    res.render("subscription", { user: req.session.user, subscriptions : subscriptions });
+  } else {
+    res.render("login", {
+      clientId: process.env.DISCORD_ID,
+      redirectUri: process.env.REDIRECT_URI,
+    });
+  }
+});
+
+app.get("/transactions", async (req, res) => {
+  if (req.session.user) {
+    const payments = await getAllLastestPayments();
+    res.render("transaction", { user: req.session.user, payments : payments });
   } else {
     res.render("login", {
       clientId: process.env.DISCORD_ID,
