d8ead71c5d9bd66b884bbd1ee4c48df279b0761f
vgabin <gabin.vallee@outlook.fr>
2025-06-05 19:38:51 +0200

    Add middleware

--- web/server.js
+++ web/server.js
@@ -6,8 +6,9 @@ const { getAllSubscriptions } = require("./src/services/subscriptions")
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 
-const authenticationRoutes = require("./src/routes/authentication");
-const apiRoutes = require("./src/routes/api");
+const authenticationRoutes  = require("./src/routes/authentication");
+const apiRoutes             = require("./src/routes/api");
+const authorizedUsers       = process.env.AUTHORIZED_USERS ? process.env.AUTHORIZED_USERS.split(",").map(u => u.trim()) : [];
 
 app.use(
   session({
@@ -21,39 +22,33 @@ app.set('view engine', 'pug');
 app.set('views', './src/views');
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
+function checkAuthorization(req, res, next) {
   if (req.session.user) {
-    res.render("index", { user: req.session.user });
+    if (!req.session.user || !authorizedUsers.includes(req.session.user.username)) {
+      return res.status(403).render("unauthorized", { message: "Accès refusé." });
+    }
+
+    next();
   } else {
     res.render("login", {
       clientId: process.env.DISCORD_ID,
       redirectUri: process.env.REDIRECT_URI,
     });
   }
+}
+
+app.get("/", checkAuthorization, (req, res) => {
+  res.render("index", { user: req.session.user });
 });
 
-app.get("/abonnements", async (req, res) => {
-  if (req.session.user) {
-    const subscriptions = await getAllSubscriptions();
-    res.render("subscription", { user: req.session.user, subscriptions : subscriptions });
-  } else {
-    res.render("login", {
-      clientId: process.env.DISCORD_ID,
-      redirectUri: process.env.REDIRECT_URI,
-    });
-  }
+app.get("/abonnements", checkAuthorization, async (req, res) => {
+  const subscriptions = await getAllSubscriptions();
+  res.render("subscription", { user: req.session.user, subscriptions : subscriptions });
 });
 
-app.get("/transactions", async (req, res) => {
-  if (req.session.user) {
-    const payments = await getAllLastestPayments();
-    res.render("transaction", { user: req.session.user, payments : payments });
-  } else {
-    res.render("login", {
-      clientId: process.env.DISCORD_ID,
-      redirectUri: process.env.REDIRECT_URI,
-    });
-  }
+app.get("/transactions", checkAuthorization, async (req, res) => {
+  const payments = await getAllLastestPayments();
+  res.render("transaction", { user: req.session.user, payments : payments });
 });
 
 app.use("/oauth2", authenticationRoutes);
